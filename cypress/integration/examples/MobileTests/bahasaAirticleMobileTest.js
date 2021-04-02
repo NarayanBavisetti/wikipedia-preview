fd57c5e7400cca89399863b1773dce7161a7e792
narayan <narayan3119@gmail.com>
2021-04-03 00:12:00 +0530

    added sample mobile test

---
+++ cypress/integration/examples/MobileTests/bahasaAirticleMobileTest.js
@@ -0,0 +1,115 @@
+import { Homepage } from "../../pageObject/HomePage";
+import { AirticelPage } from "../../pageObject/airticlePage";
+
+const homepage = new Homepage();
+const airticlePage = new AirticelPage();
+
+describe("Wikimedia english page test", function () {
+
+beforeEach( () => {
+    cy.viewport('iphone-xr')
+    cy.getHomePage("/articles/bahasaIndonesia.html");  
+})
+  it("check the header", () => {
+    // cy.getHomePage("/articles/english.html");
+    airticlePage.getHeader().should("have.text", "Wikipedia Preview demo");
+    airticlePage
+      .getHeader()
+      .should("be.visible")
+      .should("have.css", "font-size", "18px");
+    airticlePage.getHeaderLink().should("have.attr", "href", "../index.html");
+  });
+  it("check the title", () => {
+    airticlePage.getTitle().should("have.text", "Gili Trawangan");
+  });
+  it("check the cover image", () => {
+    airticlePage.getCover().should("be.visible");
+  });
+  it("Check the body", () => {
+    airticlePage.getPara1().should("have.css", "font-size", "16px");
+    airticlePage.getPara2().should("have.css", "font-size", "16px");
+  });
+
+  it("check the popups Lombok", () => {
+    airticlePage.getSpan1A().should("have.text", "Lombok");
+    airticlePage
+      .getSpan1A()
+      .should(
+        "have.attr",
+        "href",
+        "https://id.wikipedia.org/wiki/Pulau_Lombok"
+      );
+    airticlePage.getSpan1A().trigger("mouseenter")
+    cy.wait(2000)
+    cy.popUpBoxHeaderImg().should("be.visible");
+    cy.popUpBoxCloseBtn().should("be.visible");
+    cy.contains("Lanjutkan Membaca").click();
+    cy.wait(2000)
+    cy.popUpBoxFooterImg1().scrollIntoView().should("be.visible");
+    cy.popUpBoxFooterImg2().should("be.visible");
+    cy.popUpBoxFooterImg3().should("be.visible");
+    cy.contains("Baca lebih lanjut tentang Wikipedia").should("be.visible");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://id.wikipedia.org/wiki/Pulau_Lombok?wprov=wppw1"
+    );
+    cy.popUpBoxCloseBtn().click();
+  });
+  it("check the popup Gili Meno", () => {
+    airticlePage.getSpan2().should("have.text", "Gili Meno");
+    airticlePage.getSpan2().click({ force: true })
+    cy.wait(2000)
+    cy.popUpBoxHeaderImg().should("be.visible");
+    cy.popUpBoxCloseBtn().should("be.visible");
+    cy.contains("Lanjutkan Membaca").click();
+    cy.wait(2000)
+    cy.popUpBoxFooterImg1().scrollIntoView().should("be.visible");
+    cy.popUpBoxFooterImg2().should("be.visible");
+    cy.popUpBoxFooterImg3().should("be.visible");
+    cy.contains("Baca lebih lanjut tentang Wikipedia").should("be.visible");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://id.wikipedia.org/wiki/Gili_Meno?wprov=wppw1"
+    );
+    cy.popUpBoxCloseBtn().click();
+  });
+
+  it("check the footer", () => {
+    airticlePage
+      .getFooter()
+      //   <a href="" target="_blank" data-wp-title="Gili_Trawangan" data-wp-lang="id"></a>
+      .contains("Gili Trawangan")
+      .trigger("mouseenter")
+      
+      .should(
+        "have.attr",
+        "href",
+        "https://id.m.wikipedia.org/wiki/Gili_Trawangan"
+      );
+      cy.wait(2000)
+    cy.popUpBoxHeaderImg().should("be.visible");
+    cy.popUpBoxCloseBtn().should("be.visible");
+    //   cy.popUpBoxHeader()
+    cy.contains("Lanjutkan Membaca").click();
+    cy.popUpBoxFooterImg1().scrollIntoView().should("be.visible");
+    cy.popUpBoxFooterImg2().should("be.visible");
+    cy.popUpBoxFooterImg3().should("be.visible");
+    cy.contains("Baca lebih lanjut tentang Wikipedia").should("be.visible");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://id.wikipedia.org/wiki/Gili_Trawangan?wprov=wppw1"
+    );
+    cy.popUpBoxCloseBtn().click();
+    airticlePage
+      .getFooter()
+      .contains("View Source")
+      .should(
+        "have.attr",
+        "href",
+        "https://github.com/wikimedia/wikipedia-preview"
+      );
+  });
+});
---
+++ cypress/integration/examples/MobileTests/englishAirticleMobileTest.js
@@ -0,0 +1,95 @@
+import { Homepage } from "../../pageObject/HomePage";
+import { AirticelPage } from "../../pageObject/airticlePage";
+
+const homepage = new Homepage();
+const airticlePage = new AirticelPage();
+
+describe("Wikimedia english page test", function () {
+  before(function () {
+    cy.getHomePage("/articles/english.html");
+  });
+  beforeEach( () => {
+    cy.viewport('iphone-xr')
+    cy.getHomePage("/articles/english.html");  
+})
+
+  it("check the header", () => {
+    // cy.getHomePage("/articles/english.html");
+    airticlePage.getHeader().should("have.text", "Wikipedia Preview demo");
+    airticlePage
+      .getHeader()
+      .should("be.visible")
+      .should("have.css", "font-size", "18px");
+    airticlePage.getHeaderLink().should("have.attr", "href", "../index.html");
+  });
+  it("check the title", () => {
+    airticlePage
+      .getTitle()
+      .should("have.text", "Wildlife of the Central African Republic");
+  });
+  it("check the cover image", () => {
+    airticlePage.getCover().should("be.visible");
+  });
+  it("Check the body", () => {
+    airticlePage.getPara1().should("have.css", "font-size", "16px");
+    airticlePage.getPara2().should("have.css", "font-size", "16px");
+    airticlePage.getPara3().should("have.css", "font-size", "16px");
+  });
+
+  it("check all the popups in loop", () => {
+    cy.openingApopups("ivory");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://en.wikipedia.org/wiki/Ivory?wprov=wppw1"
+    );
+    // will make a method to check third picture
+    cy.openingApopups("Bamingui-Bangoran National");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://en.wikipedia.org/wiki/Bamingui-Bangoran_National_Park_and_Biosphere_Reserve?wprov=wppw1"
+    );
+    cy.openingApopups("Chinko Project");
+    cy.popUpBoxFoooterLink().should(
+      "have.attr",
+      "href",
+      "https://en.wikipedia.org/wiki/Chinko?wprov=wppw1"
+    );
+    // cy.openingApopups('50')
+  });
+
+  // it("check the popup 50 ", () => {
+  //   cy.get("span.wmf-wp-with-preview").contains("50").click({ force: true });
+  //   cy.wait(2000);
+  //   cy.popUpBox().should("be.visible");
+  //   cy.popUpBoxCloseBtn().should("be.visible");
+  //   cy.contains("Read on Wikipedia").should("be.visible");
+  //   cy.get(
+  //     "body > div.wp-popup > div > div.wikipediapreview-body.wikipediapreview-body-disambiguation > div.wikipediapreview-body-action > a"
+  //   ).should(
+  //     "have.attr",
+  //     "href",
+  //     "https://en.wikipedia.org/wiki/50?wprov=wppw1"
+  //   );
+  //   cy.popUpBoxCloseBtn().click();
+  // });
+  it("check the footer", () => {
+    airticlePage
+      .getFooter()
+      .contains("Wildlife of the Central African Republic")
+      .should(
+        "have.attr",
+        "href",
+        "https://en.m.wikipedia.org/wiki/Wildlife_of_the_Central_African_Republic"
+      );
+    airticlePage
+      .getFooter()
+      .contains("View Source")
+      .should(
+        "have.attr",
+        "href",
+        "https://github.com/wikimedia/wikipedia-preview"
+      );
+  });
+});
